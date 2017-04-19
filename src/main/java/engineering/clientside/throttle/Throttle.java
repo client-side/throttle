990c6c37c96f20021875b3cc044773c755aec632
jamespedwards42 <james.p.edwards42@gmail.com>
2017-04-19 08:20:10 -0400

    Fix missing throws javadoc.

--- src/main/java/engineering/clientside/throttle/Throttle.java
+++ src/main/java/engineering/clientside/throttle/Throttle.java
@@ -276,6 +276,7 @@ public interface Throttle {
    * @return The duration in nanosecond to wait to match the acquired permits, or -1 if no permits
    * were acquired because the timeout would expire.
    * @throws IllegalArgumentException if the requested number of permits is negative or zero
+   * @throws InterruptedException unchecked internally if thread is interrupted
    */
   long tryAcquireDelayDuration(final int permits, final long timeout, final TimeUnit unit);
 
